d3d9efbe9928ce475a835c2f36f1b4c6f335e351
riptide_wave <chris@servernetworktech.com>
2016-04-26 20:06:21 -0500

    Fixup packages
    Add the packages I personally use

--- overlay/target/linux/x86/64/profiles/001-pcengine.mk
+++ overlay/target/linux/x86/64/profiles/001-pcengine.mk
@@ -7,11 +7,22 @@
 
 define Profile/APU2
   NAME:=PC Engines APU2
-  PACKAGES:=kmod-igb hwclock kmod-button-hotplug kmod-i2c-core \
+  PACKAGES:=kmod-igb hwclock kmod-i2c-core \
   	kmod-i2c-piix4 kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb3 \
   	kmod-ath9k kmod-ath10k wpad-mini kmod-hwmon-core kmod-hwmon-k10temp \
-	kmod-leds-gpio kmod-gpio-button-hotplug \
-  	libsensors lm-sensors
+	kmod-crypto-hw-ccp kmod-crypto-hw-geode \
+	kmod-leds-gpio kmod-button-hotplug kmod-gpio-button-hotplug \
+	flashrom flashrom-spi libsensors lm-sensors \
+	kmod-usb-uhci kmod-usb-storage kmod-usb-storage-extras block-mount \
+	kmod-fs-ntfs kmod-fs-vfat kmod-fs-msdos relayd htop nano \
+	transmission-daemon-openssl transmission-cli-openssl transmission-web \
+	collectd collectd-mod-cpu collectd-mod-interface collectd-mod-iwinfo \
+	collectd-mod-load collectd-mod-memory collectd-mod-network \
+	collectd-mod-ping collectd-mod-rrdtool collectd-mod-wireless \
+	luci luci-ssl luci-theme-material luci-proto-relay \
+	luci-app-commands luci-app-firewall luci-app-openvpn luci-app-samba \
+	luci-app-statistics luci-app-transmission luci-app-upnp \
+	luci-app-vnstat
 endef
 
 define Profile/APU2/Description
